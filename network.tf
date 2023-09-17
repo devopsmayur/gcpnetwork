b538c8bc7f12472efa7b0cb0f151d037ffd38210
Mayur <99334268+devopsmayur@users.noreply.github.com>
2023-09-17 10:31:48 +0530

    Update network.tf

--- network.tf
+++ network.tf
@@ -1,1 +1,18 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+}
 
+provider "google" {
+  project = "hc-430e7eefb27e4cc18e75a35037a"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
